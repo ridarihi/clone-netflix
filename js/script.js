47e4e38c11e86d0cef2d8a9a2b749f93739511d0
Rida Rihi <162450697+ridarihi@users.noreply.github.com>
2024-07-22 17:21:17 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,26 @@
+/*console.log("hello in netflix")
+document.write("<h1>netflix</h1>")
+window.alert("stop netflix")*/
+/*console.error("error to sanyx")*/
+/*console.log("rida%c FLIX %c maroc","color:red ; font-size:30px; background-color:black;","color:blue");
+*/
+/*var Myname = "100" ;
+console.log ("My name is " + Myname);
+console.log(typeof Myname);
+console.log (typeof 200);
+var data = ["rida", "rihi" , "maroc"]
+console.log (typeof data)
+console.log (typeof false)
+var data1 ="{name:'rida',age:20, contry:'morocco'}"
+console.log (typeof {data1})
+console.log ("My name is " + Myname);
+console.log ("My name is " + Myname);
+console.log ("My name is " + Myname);
+console.log ("My name is " + Myname);
+console.log ("My name is " + Myname);
+user.innerHTML = "rida"
+var element =user.innerHTML = "rida"
+element.style.color = "red"
+
+
+
